b741e8d049bd38585bb28c9304e32115d8436c3d
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro-2.local>
2016-03-16 11:57:07 -0500

    this is not just a landing page

--- app/controllers/finder-ctrl.js
+++ app/controllers/finder-ctrl.js
@@ -3,18 +3,47 @@
 app.controller("FinderCtrl", [
 	'$scope',
 	'get-movies',
+	'post-requests',
+	'authenticate',
 
-	function ($scope, getMovies) {
+	function ($scope, getMovies, postRequests, authenticate) {
+
+		$scope.movieWasSearched = false;
+
+		$scope.movieToAdd = {
+			poster: "",
+			title: "",
+			year: "",
+			actors: [],
+			user: "-KCwF1J8j17zjGJxCGq_",
+			rating: 0,
+			watched: false,
+		}
+
 		$scope.movieToSearch = "";
+
 		$scope.search = () => {
 			let searchTerm = $scope.movieToSearch;
 			getMovies(searchTerm)
 			.then(
 				(movieData) => {
+					$scope.movieWasSearched = true;
 					$('#card').append(`<img src=${movieData.Poster} />`);
-					$('#card').append(`<a href="#">Add To Watchlist</a>`);
+					$scope.movieToAdd.poster = movieData.Poster;
+					$scope.movieToAdd.title = movieData.Title;
+					$scope.movieToAdd.year = movieData.Year;
+					$scope.movieToAdd.actors = movieData.Actors.split(',');
 				}
 			)
 		}
+
+		$scope.addMovie = () => {
+			postRequests.postMovie($scope.movieToAdd)
+			.then(
+				(movieData) => console.log('movie saved!'),
+				(error) => console.log(error)
+			)
+		}
+
 	}]
 );
